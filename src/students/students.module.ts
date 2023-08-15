e9460a2f0a7c05c044d12e2b366eea07dc92d9ec
osama <osama@platformcommons.com>
2023-08-15 12:18:54 +0530

    Middle ware authorization added in Post and Delete API

--- src/students/students.module.ts
+++ src/students/students.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
+import { StudentsMiddleware } from './students.middleware';
 
 @Module({
   controllers: [StudentsController],
   providers: [StudentsService],
 })
-export class StudentsModule {}
+export class StudentsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // Either pass routes or controller in apply(Path || Controller) 
+    consumer.apply(StudentsMiddleware)
+      .exclude(
+        { path: 'students', method: RequestMethod.GET },
+        { path: 'students/(.*)', method: RequestMethod.GET },
+      )
+      .forRoutes(StudentsController)
+  }
+}
